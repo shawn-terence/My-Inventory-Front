d9ea36d6d56a7aa509242ba83b0bec06d044939b
terence <terenceshawn18@outlook.com>
2024-08-27 10:47:22 +0300

    Chore:Customizing Default.tsx

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,6 @@
 import { Link } from "@nextui-org/link";
-
 import { Navbar } from "@/components/navbar";
+import SideBar from '../components/SideBar'
 
 export default function DefaultLayout({
   children,
@@ -10,20 +10,25 @@ export default function DefaultLayout({
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
-        {children}
-      </main>
-      <footer className="w-full flex items-center justify-center py-3">
+      <div id="container" className="flex  flex-grow ">
+        <SideBar/> 
+        <main id="MainContainer" className=" bg-white container  h-fit pb-10 border shadow-lg rounded-xl mx-auto max-w-8xl px-6 flex-grow  ">
+          {children}
+        </main>
+      </div>
+      <footer id="Footer" className="w-full flex items-center justify-center py-3">
         <Link
+          
           isExternal
           className="flex items-center gap-1 text-current"
           href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
           title="nextui.org homepage"
         >
-          <span className="text-default-600">Powered by</span>
+          <span className="text-default-600">My Inventory &#169; 2024</span>
           <p className="text-primary">NextUI</p>
         </Link>
       </footer>
     </div>
   );
 }
+
