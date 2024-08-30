b584ef5e9c8895baeba8b3d03fbdceb3f5e6331f
terence <terenceshawn18@outlook.com>
2024-08-30 15:57:32 +0300

    Chore:Fixing chart layout

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -20,7 +20,7 @@ const BarChart = ({ ChartData }) => {
   }
 
   return (
-    <div className='m-4 w-full p-2'>
+    <div className='m-4 p-2'>
       <Bar
         id='BarChart'
         className='Bar-Chart'
