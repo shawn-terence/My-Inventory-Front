6b23e454be745e5ec3d6d92a70f3e192bdcd19a7
terence <terenceshawn18@outlook.com>
2024-08-30 14:17:10 +0300

    Chore:Updating barchart sizes

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -19,13 +19,13 @@ function Statistics() {
         <Tabs>
           <Tab key="Sales" title="Sale Statistics">
             <h2 className="text-center">Sales Statistics for last month and this month</h2>
-            <div className="flex justify-between">
+            <div className="flex justify-around bg-danger">
               <MonthlySale />
               <CurrentSale />
             </div>
             <div>
               <h2 className="text-center">Highest and Lowest Sales</h2>
-              <div className="flex justify-between">
+              <div className="flex justify-around">
                 <HighSale />
                 <LowSale />
               </div>
